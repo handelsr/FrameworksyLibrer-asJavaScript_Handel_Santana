ba53420ebb33333b04b49bc6bd73466089077883
handelsr <31743655+handelsr@users.noreply.github.com>
2018-10-10 22:31:42 -0400

    Edicion 3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 var juegoActivo = 0
+    movimientos = 1
+    puntuacion = 0
+    totalImagenes = 35
 var data = [];
 
 var juegoDulces = {
@@ -34,5 +37,34 @@ var juegoDulces = {
                 juegoDulces.animacionAcertar();
             }, 800);
         }
+    },
+    terminarJuego: function () {
+        $(".panel-tablero").hide("slide", { direction: "left" }, "slow", function () {
+            $(".time").hide("slide", { direction: "left" }, "slow");
+            $(".panel-score").animate({ width: "390%" }, 1000);
+            if (juegoActivo != 1) {
+                $(".panel-score").prepend('<h1 class="main-titulo-2">Juego Terminado</h1>');
+            }
+        });
+    },
+    reiniciarJuego: function () {
+        var col = $("div[class^='col']");
+        for (var i = 0; i < col.length; i++) {
+            $(col[i]).html('');
+        }
+        $('#timer').timer('remove');
+        $(".btn-reinicio").html('Iniciar');
+        $("#timer").html('02:00');
+        $("#movimientos-text").html('0');
+        $("#score-text").html('0');
+        puntuacion = 0;
+        movimientos = 1;
+        this.cargarImagenes();
+        $(".main-titulo-2").remove();
+        $(".panel-score").animate({ width: "25%" }, 1000, function () {
+            $(".panel-tablero").show("slide", { direction: "left" }, "slow");
+            $(".time").show("slide", { direction: "left" }, "slow");
+        });
+        juegoActivo = 0;
     },
 }

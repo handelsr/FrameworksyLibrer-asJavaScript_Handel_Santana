5569272d70b9ad3ef3344bbc219fdb8441978c2b
handelsr <31743655+handelsr@users.noreply.github.com>
2018-10-10 22:37:18 -0400

    Edicion 4

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 var juegoActivo = 0
     movimientos = 1
     puntuacion = 0
-    totalImagenes = 35
+totalImagenes = 35
+totalFilas = 5
+totalColumnas = 7
+bloqueo = 0
 var data = [];
 
 var juegoDulces = {
@@ -66,5 +69,98 @@ var juegoDulces = {
             $(".time").show("slide", { direction: "left" }, "slow");
         });
         juegoActivo = 0;
+    },
+    inicioTiempo: function () {
+        $('#timer').timer({
+            //http://jquerytimer.com/
+            duration: '2m',
+            format: '%M:%S',
+            callback: function () {
+                juegoActivo = 2;
+                juegoDulces.terminarJuego();
+            }
+        });
+    },
+    animacionTitulo: function () {
+        setInterval(function () {
+            $(".main-titulo").switchClass("main-titulo", "main-titulo-efecto", 200),
+                $(".main-titulo").switchClass("main-titulo-efecto", "main-titulo", 200)
+        }, 1000);
+    },
+    imagenes: function () {
+        var i = 0;
+        this[i++] = "image/1.png";
+        this[i++] = "image/2.png";
+        this[i++] = "image/3.png";
+        this[i++] = "image/4.png";
+        this.total = i;
+    },
+    obtenerColumnas: function () {
+        var i = 0;
+        this[i++] = $(".col-1").find("div");
+        this[i++] = $(".col-2").find("div");
+        this[i++] = $(".col-3").find("div");
+        this[i++] = $(".col-4").find("div");
+        this[i++] = $(".col-5").find("div");
+        this[i++] = $(".col-6").find("div");
+        this[i++] = $(".col-7").find("div");
+        this.total = i;
+    },
+    obtenerFilas: function () {
+        var i = 0;
+        this[i++] = $("[id*=img-1]");
+        this[i++] = $("[id*=img-2]");
+        this[i++] = $("[id*=img-3]");
+        this[i++] = $("[id*=img-4]");
+        this[i++] = $("[id*=img-5]");
+        this.total = i;
+    },
+    cargarImagenes: function () {
+        var num = 1;
+        var numImg = 1;
+        for (var i = 1; i <= totalColumnas; i++) {
+            for (var ii = 1; ii <= totalFilas; ii++) {
+                var imagen = new this.imagenes;
+                var src = imagen[Math.floor(Math.random() * imagen.total)];
+                $(".col-" + num).append("<div id='item-" + numImg + " img-" + ii + "'>" +
+                    "<img src=" + src + " class='imagen-" + numImg + "'>" +
+                    "</div>");
+                $(".imagen-" + numImg).draggable({
+                    revert: true,
+                    containment: ".panel-tablero",
+                    start: function () {
+                        if (juegoActivo == 1) {
+                            $("#movimientos-text").html(movimientos++);
+                        }
+                    },
+                    stop: function () {
+                        if (juegoActivo == 1) {
+                            if (bloqueo == 0) {
+                                juegoDulces.scanRepetidosCol();
+                                juegoDulces.scanRepetidosFil();
+                                juegoDulces.animacionAcertar();
+                            }
+                        }
+                    },
+                    drag: function (event, ui) { }
+                });
+                $("[id='item-" + numImg + " img-" + ii + "'").droppable({
+                    drop: function (event, ui) {
+                        if (juegoActivo == 1) {
+                            if (bloqueo == 0) {
+                                imagenUno = event.target.lastChild;
+                                imagenDos = ui.draggable[0];
+                                imgUno = $(imagenUno).attr("src");
+                                imgDos = $(imagenDos).attr("src");
+                                $(imagenUno).attr("src", imgDos);
+                                $(imagenDos).attr("src", imgUno);
+                            }
+                        }
+                    }
+                });
+                numImg++;
+            }
+            num++;
+        }
     },
 }
